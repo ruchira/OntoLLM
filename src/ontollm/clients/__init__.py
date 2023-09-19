c827000834df8cca0eeb4bb6e64656656c5e413a
Ruchira S Datta rsd11 <rdatta@cas.org>
2023-09-19 18:09:17 +0000

    Tried adding more path specification instead of less to the imports.

--- src/ontollm/clients/__init__.py
+++ src/ontollm/clients/__init__.py
@@ -1,3 +1,3 @@
 """Clients for accessing different APIs."""
-from pubmed_client import PubmedClient  # noqa:F401
-from soup_client import SoupClient  # noqa:F401
+from OntoLLM.src.ontollm.pubmed_client import PubmedClient  # noqa:F401
+from OntoLLM.src.ontollm.soup_client import SoupClient  # noqa:F401
