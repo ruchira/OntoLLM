a3cc0b513547744112508025274be58b8c6af71a
Ruchira S Datta rsd11 <rdatta@cas.org>
2023-09-19 18:10:37 +0000

    Corrected paths.

--- src/ontollm/clients/__init__.py
+++ src/ontollm/clients/__init__.py
@@ -1,3 +1,3 @@
 """Clients for accessing different APIs."""
-from OntoLLM.src.ontollm.pubmed_client import PubmedClient  # noqa:F401
-from OntoLLM.src.ontollm.soup_client import SoupClient  # noqa:F401
+from OntoLLM.src.ontollm.clients.pubmed_client import PubmedClient  # noqa:F401
+from OntoLLM.src.ontollm.clients.soup_client import SoupClient  # noqa:F401
