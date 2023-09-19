b429c725409c1c70ca31f1db671a7c05605c4437
Ruchira S Datta rsd11 <rdatta@cas.org>
2023-09-19 18:07:19 +0000

    Removed unnecessary ontollm.clients from import, as we are already in ontollm.clients.

--- src/ontollm/clients/__init__.py
+++ src/ontollm/clients/__init__.py
@@ -1,3 +1,3 @@
 """Clients for accessing different APIs."""
-from ontollm.clients.pubmed_client import PubmedClient  # noqa:F401
-from ontollm.clients.soup_client import SoupClient  # noqa:F401
+from pubmed_client import PubmedClient  # noqa:F401
+from soup_client import SoupClient  # noqa:F401
