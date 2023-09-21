a30dc3e898df7d047c45095086845f7eb401bf68
Ruchira S Datta rsd11 <rdatta@cas.org>
2023-09-21 18:23:16 +0000

    Attempting to fix typo.

--- src/ontollm/clients/llama2_client.py
+++ src/ontollm/clients/llama2_client.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import sqlite3
 from typing import Iterator, Tuple
 
-from llama import llama
+from llama import Llama
 from llama.llama.tokenizer import Tokenizer
 from torch import Tensor, device
 
