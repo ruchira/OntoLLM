monarch-initiative/ontogpt

Eighth pass at type checking

src/ontogpt/engines/knowledge_engine.py
@@ -216,7 +216,7 @@ class KnowledgeEngine(ABC):
         raise NotImplementedError
 
     def generalize(
-        self, object: Union[pydantic.BaseModel, dict], examples: List[EXAMPLE]
+        self, object: Union[pydantic.BaseModel, dict], examples: List[EXAMPLE], show_prompt: bool
     ) -> ExtractionResult:
         raise NotImplementedError
 

ruchira/OntoLLM

src/ontollm/engines/knowledge_engine.py
@@ -202,7 +202,7 @@ class KnowledgeEngine(ABC):
         raise NotImplementedError
 
     def generalize(
-        self, an_object: Union[pydantic.BaseModel, dict], examples: List[EXAMPLE]
+        self, an_object: Union[pydantic.BaseModel, dict], examples: List[EXAMPLE], show_prompt: bool
     ) -> ExtractionResult:
         raise NotImplementedError
 