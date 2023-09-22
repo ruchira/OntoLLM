monarch-initiative/ontogpt

Eleventh pass at type fixes

src/ontogpt/cli.py
@@ -31,8 +31,8 @@ from ontogpt.engines import create_engine
 from ontogpt.engines.embedding_similarity_engine import SimilarityEngine
 from ontogpt.engines.enrichment import EnrichmentEngine
 from ontogpt.engines.generic_engine import GenericEngine, QuestionCollection
-from ontogpt.engines.gpt4all_engine import GPT4AllEngine
-from ontogpt.engines.halo_engine import HALOEngine
+from ontogpt.engines.gpt4all_engine import GPT4AllEngine  # type: ignore
+from ontogpt.engines.halo_engine import HALOEngine  # type: ignore
 
 # from ontogpt.engines.hfhub_engine import HFHubEngine
 from ontogpt.engines.knowledge_engine import KnowledgeEngine
@@ -105,7 +105,7 @@ def write_extraction(
             exporter.export(results, output)
         elif output_format == "yaml":
             output = _as_text_writer(output)
-            output.write(dump_minimal_yaml(results).encode('utf-8'))
+            output.write(dump_minimal_yaml(results).encode("utf-8"))
         elif output_format == "turtle":
             output = _as_text_writer(output)
             exporter = RDFExporter()
@@ -118,13 +118,13 @@ def write_extraction(
             # output = _as_text_writer(output)
             # output.write(write_obj_as_csv(results))
             output = _as_text_writer(output)
-            output.write(dump_minimal_yaml(results).encode('utf-8'))
+            output.write(dump_minimal_yaml(results).encode("utf-8"))
             with open("output.kgx.tsv") as secondoutput:
                 for line in output_parser(obj=results, file=output):
                     secondoutput.write(line)
         else:
             output = _as_text_writer(output)
-            output.write(dump_minimal_yaml(results).encode('utf-8'))
+            output.write(dump_minimal_yaml(results).encode("utf-8"))
 
 
 def get_model_by_name(modelname: str):

src/ontogpt/engines/mapping_engine.py
@@ -112,7 +112,7 @@ class MappingEngine(KnowledgeEngine):
     object_adapter: BasicOntologyInterface = None
 
     def categorize_mapping(
-        self, subject: CURIE, object: CURIE, template_path: str = ""
+        self, subject: CURIE, object: CURIE, template_path: Union[str, Path] = ""
     ) -> CategorizedMapping:
         if template_path is None:
             template_path = DEFAULT_MAPPING_EVAL_PROMPT
@@ -257,7 +257,7 @@ class MappingEngine(KnowledgeEngine):
             task = MappingTask(
                 subject=mapping.subject_id,
                 object=mapping.object_id,
-                predicate=MappingPredicate.mappings().get(mapping.predicate_id),
+                predicate=MappingPredicate.mappings().get(mapping.predicate_id),  # type: ignore
                 subject_label=mapping.subject_label,
                 object_label=mapping.object_label,
                 subject_source=mapping.subject_source,
@@ -287,11 +287,11 @@ class MappingEngine(KnowledgeEngine):
         self.subject_adapter = _get_adapter(mapping.subject_source)
         self.object_adapter = _get_adapter(mapping.object_source)
         cm = self.categorize_mapping(mapping.subject_id, mapping.object_id)
-        revmap = {v.upper(): k for k, v in MappingPredicate.mappings().items()}
+        revmap = {v.upper(): k for k, v in MappingPredicate.mappings().items()}  # type: ignore
         if cm.predicate.upper() not in revmap:
             logger.warning(f"Unknown predicate {cm.predicate}")
         mapping.predicate_id = revmap.get(cm.predicate.upper(), SKOS_RELATED_MATCH)
-        return mapping, cm
+        return mapping, cm  # type: ignore
 
     def _concept(self, curie: CURIE, adapter: BasicOntologyInterface) -> Concept:
         """Get a concept."""

src/ontogpt/engines/resolver.py
@@ -4,7 +4,7 @@ from typing import Optional, Type, Union
 from class_resolver import ClassResolver
 
 from ontogpt.engines.enrichment import EnrichmentEngine
-from ontogpt.engines.halo_engine import HALOEngine
+from ontogpt.engines.halo_engine import HALOEngine  # type: ignore
 from ontogpt.engines.knowledge_engine import KnowledgeEngine
 from ontogpt.engines.spires_engine import SPIRESEngine
 

src/ontogpt/evaluation/hpoa/eval_hpoa.py
@@ -224,9 +224,12 @@ class EvalHPOA(SPIRESEvaluationEngine):
                 pmc = PubmedClient()
                 pub_resultset = []
                 for pmid in test_case.publications:
-                    if pmid.startswith("PMID:"):
+                    if str(pmid).startswith("PMID:"):
                         pub_text = pmc.text(pmid)
-                        pub_resultset.append(ke.extract_from_text(pub_text, object=stub))
+                        if type(pub_text) is str:  # Should just be one document.
+                            pub_resultset.append(ke.extract_from_text(pub_text, object=stub))
+                        else:
+                            continue
                 results = ke.merge_resultsets([results] + pub_resultset, ["name"])
             predicted_obj = results.extracted_object
             pred = PredictionHPOA(predicted_object=predicted_obj, test_object=test_case)

ruchira/OntoLLM

src/ontollm/cli.py
@@ -30,8 +30,8 @@ from ontollm.engines import create_engine
 from ontollm.engines.embedding_similarity_engine import SimilarityEngine
 from ontollm.engines.enrichment import EnrichmentEngine
 from ontollm.engines.generic_engine import GenericEngine, QuestionCollection
-from ontollm.engines.gpt4all_engine import GPT4AllEngine
-from ontollm.engines.halo_engine import HALOEngine
+from ontollm.engines.gpt4all_engine import GPT4AllEngine  # type: ignore
+from ontollm.engines.halo_engine import HALOEngine  # type: ignore
 
 # from ontollm.engines.hfhub_engine import HFHubEngine
 from ontollm.engines.knowledge_engine import KnowledgeEngine
@@ -43,6 +43,7 @@ from ontollm.engines.synonym_engine import SynonymEngine
 from ontollm.evaluation.enrichment.eval_enrichment import EvalEnrichment
 from ontollm.evaluation.resolver import create_evaluator
 from ontollm.io.csv_wrapper import write_obj_as_csv
+from ontollm.io.csv_wrapper import output_parser
 from ontollm.io.html_exporter import HTMLExporter
 from ontollm.io.markdown_exporter import MarkdownExporter
 from ontollm.utils.gene_set_utils import (
@@ -103,7 +104,7 @@ def write_extraction(
             exporter.export(results, output)
         elif output_format == "yaml":
             output = _as_text_writer(output)
-            output.write(dump_minimal_yaml(results).encode('utf-8'))
+            output.write(dump_minimal_yaml(results).encode("utf-8"))
         elif output_format == "turtle":
             output = _as_text_writer(output)
             exporter = RDFExporter()
@@ -116,13 +117,13 @@ def write_extraction(
             # output = _as_text_writer(output)
             # output.write(write_obj_as_csv(results))
             output = _as_text_writer(output)
-            output.write(dump_minimal_yaml(results).encode('utf-8'))
+            output.write(dump_minimal_yaml(results).encode("utf-8"))
             with open("output.kgx.tsv") as secondoutput:
                 for line in output_parser(obj=results, file=output):
                     secondoutput.write(line)
         else:
             output = _as_text_writer(output)
-            output.write(dump_minimal_yaml(results).encode('utf-8'))
+            output.write(dump_minimal_yaml(results).encode("utf-8"))
 
 
 def get_model_by_name(modelname: str):

src/ontollm/engines/mapping_engine.py
@@ -112,7 +112,7 @@ class MappingEngine(KnowledgeEngine):
     object_adapter: BasicOntologyInterface = None
 
     def categorize_mapping(
-        self, subject: CURIE, ont_object: CURIE, template_path: str = None
+        self, subject: CURIE, ont_object: CURIE, template_path: Union[str, Path] = ""
     ) -> CategorizedMapping:
         if template_path is None:
             template_path = DEFAULT_MAPPING_EVAL_PROMPT
@@ -257,7 +257,7 @@ class MappingEngine(KnowledgeEngine):
             task = MappingTask(
                 subject=mapping.subject_id,
                 ont_object=mapping.object_id,
-                predicate=MappingPredicate.mappings().get(mapping.predicate_id),
+                predicate=MappingPredicate.mappings().get(mapping.predicate_id),  # type: ignore
                 subject_label=mapping.subject_label,
                 object_label=mapping.object_label,
                 subject_source=mapping.subject_source,
@@ -287,11 +287,11 @@ class MappingEngine(KnowledgeEngine):
         self.subject_adapter = _get_adapter(mapping.subject_source)
         self.object_adapter = _get_adapter(mapping.object_source)
         cm = self.categorize_mapping(mapping.subject_id, mapping.object_id)
-        revmap = {v.upper(): k for k, v in MappingPredicate.mappings().items()}
+        revmap = {v.upper(): k for k, v in MappingPredicate.mappings().items()}  # type: ignore
         if cm.predicate.upper() not in revmap:
             logger.warning(f"Unknown predicate {cm.predicate}")
         mapping.predicate_id = revmap.get(cm.predicate.upper(), SKOS_RELATED_MATCH)
-        return mapping, cm
+        return mapping, cm  # type: ignore
 
     def _concept(self, curie: CURIE, adapter: BasicOntologyInterface) -> Concept:
         """Get a concept."""

src/ontollm/engines/resolver.py
@@ -4,7 +4,7 @@ from typing import Optional, Type, Union
 from class_resolver import ClassResolver
 
 from ontollm.engines.enrichment import EnrichmentEngine
-from ontollm.engines.halo_engine import HALOEngine
+from ontollm.engines.halo_engine import HALOEngine  # type: ignore
 from ontollm.engines.knowledge_engine import KnowledgeEngine
 from ontollm.engines.spires_engine import SPIRESEngine
 

src/ontollm/evaluation/hpoa/eval_hpoa.py
@@ -222,9 +222,12 @@ class EvalHPOA(SPIRESEvaluationEngine):
                 pmc = PubmedClient()
                 pub_resultset = []
                 for pmid in test_case.publications:
-                    if pmid.startswith("PMID:"):
+                    if str(pmid).startswith("PMID:"):
                         pub_text = pmc.text(pmid)
-                        pub_resultset.append(ke.extract_from_text(pub_text, an_object=stub))
+                        if type(pub_text) is str:  # Should just be one document.
+                            pub_resultset.append(ke.extract_from_text(pub_text, an_object=stub))
+                        else:
+                            continue
                 results = ke.merge_resultsets([results] + pub_resultset, ["name"])
             predicted_obj = results.extracted_object
             pred = PredictionHPOA(predicted_object=predicted_obj, test_object=test_case)